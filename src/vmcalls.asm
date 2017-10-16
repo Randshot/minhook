d330d4238e4466c565ce353fec2a329cae51d9db
nick-kvmhv <nick.kvmhv@gmail.com>
2017-10-16 12:54:37 -0700

    Update for compatible API

--- src/vmcalls.asm
+++ src/vmcalls.asm
@@ -5,17 +5,22 @@ PUBLIC deactivatePagehv
 PUBLIC deactivateAllPageshv
 PUBLIC isPageSplithv
 PUBLIC writeCodePagehv
+PUBLIC setAdjusterhv
 
 .CODE
+MAGIC_NUMBER:
+dq 0B045EACDACD52E22h
 ALIGN     8
 checkhv PROC FRAME
-.ENDPROLOG
 push rbx
-mov ax,0005h
+.ENDPROLOG
+mov rax,2
+mov rdx,QWORD PTR [MAGIC_NUMBER]
+mov rcx,0000h
 vmcall
+mov rax,rcx
 pop rbx
 ret
-ALIGN     8
 checkhv ENDP
 ALIGN     8
 
@@ -23,10 +28,12 @@ setDataPagehv PROC FRAME
 ;void* pageAddr, void* data
 push rbx
 .ENDPROLOG
+mov rax,2
+mov rdx,QWORD PTR [MAGIC_NUMBER]
 mov rbx,rcx
-; rdx should already have it
-mov ax,0001h
+mov rcx,0001h
 vmcall
+mov rax,rcx
 pop rbx
 ret
 ALIGN     8
@@ -36,47 +43,59 @@ activatePagehv PROC FRAME
 ;void* pageAddr
 push rbx
 .ENDPROLOG
+mov rax,2
+mov rdx,QWORD PTR [MAGIC_NUMBER]
 mov rbx,rcx
-mov ax,0002h
+mov rcx,0002h
 vmcall
+mov rax,rcx
 pop rbx
 ret
 ALIGN     8
 activatePagehv ENDP
 
 writeCodePagehv PROC FRAME
-;void* pageAddr
+;rcx, rdx, r8 from,to,bytes
+push rsi
 push rbx
 .ENDPROLOG
-;mov rcx,rcx
-;mov rdx,rdx
-;mov r8,r8
-mov ax,0003h
+mov rax,2
+mov rsi, rdx
+mov rdx,QWORD PTR [MAGIC_NUMBER]
+mov rbx, rcx
+;mov r8, r8
+mov rcx,0006h
 vmcall
+mov rax,rcx
 pop rbx
+pop rsi
 ret
 ALIGN     8
 writeCodePagehv ENDP
-
-
 
 deactivatePagehv PROC FRAME
 ;void* pageAddr
 push rbx
 .ENDPROLOG
+mov rax,2
+mov rdx,QWORD PTR [MAGIC_NUMBER]
 mov rbx,rcx
-mov ax,0004h
+mov rcx,0003h
 vmcall
+mov rax,rcx
 pop rbx
 ret
 ALIGN     8
 deactivatePagehv ENDP
 
 deactivateAllPageshv PROC FRAME
-.ENDPROLOG
 push rbx
-mov ax,0006h
+.ENDPROLOG
+mov rax,2
+mov rdx,QWORD PTR [MAGIC_NUMBER]
+mov rcx,0004h
 vmcall
+mov rax,rcx
 pop rbx
 ret
 ALIGN     8
@@ -84,15 +103,38 @@ deactivateAllPageshv ENDP
 ALIGN     8
 
 isPageSplithv PROC FRAME
-.ENDPROLOG
 push rbx
-mov ax,0007h
+.ENDPROLOG
+mov rax,2
+mov rdx,QWORD PTR [MAGIC_NUMBER]
+mov rbx, rcx
+mov rcx,0005h
 vmcall
+mov rax,rcx
 pop rbx
 ret
 ALIGN     8
 isPageSplithv ENDP
 ALIGN     8
+
+setAdjusterhv PROC FRAME
+;rcx, rdx, r8 from,to,by
+push rsi
+push rbx
+.ENDPROLOG
+mov rax,2
+mov rsi, rdx
+mov rdx,QWORD PTR [MAGIC_NUMBER]
+mov rbx, rcx
+;mov r8, r8
+mov rcx,1000h
+vmcall
+mov rax,rcx
+pop rbx
+pop rsi
+ret
+ALIGN     8
+setAdjusterhv ENDP
 
 _TEXT ENDS
 END
